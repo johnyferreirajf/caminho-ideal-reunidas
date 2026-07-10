c2e38b538b66e0415d6f2a5adfbbbcab6bb5eebd
johnyferreirajf <johnyferreira.jf@gmail.com>
2026-07-10 10:53:29 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='caminho-ideal-v10-9-menu-mobile-compacto';
+const CACHE='caminho-ideal-v11-9-menu-mobile-compacto';
 const APP=['./','./index.html','./manifest.webmanifest','https://unpkg.com/leaflet@1.9.4/dist/leaflet.js','https://unpkg.com/leaflet@1.9.4/dist/leaflet.css'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(APP.map(u=>c.add(u)))).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
