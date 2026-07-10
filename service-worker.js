16dc81e7db7f17e477b1371aa4e77a18d047d17a
johnyferreirajf <johnyferreira.jf@gmail.com>
2026-07-10 11:14:06 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='caminho-ideal-v12-roads-route-fix';
+const CACHE_NAME='caminho-ideal-v13-roads-route-fix';
 const APP_ASSETS=['./','./index.html','./manifest.webmanifest','./diagnostico_conexao_fazendas.csv','./LEIA-ME.txt'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
