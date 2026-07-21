13922508cab3a819f401a98f5bb6879e7c26ea35
johnyferreirajf <johnyferreira.jf@gmail.com>
2026-07-21 13:06:18 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='trajeto-reunidas-pontal-v6';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./logo_reunidas_pontal.png','./coordenadas_centros_poligonos.csv'];
+const CACHE_NAME='trajeto-origem-goiatuba-v7';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.png','./coordenadas_centros_poligonos.csv'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE_NAME).then(c=>c.put(e.request,copy)).catch(()=>{}); return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
